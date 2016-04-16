66fd6712598e37c70b37ec02a0eafb9c80e8b576
NailMan <lehuading@qq.com>
2016-04-16 22:42:02 +0800

    get data

---
+++ README.md
@@ -0,0 +1,14 @@
+# 数据说明
+
+所有数据均放在data目录下，以JSON格式存放。
+
++ 计算机类数据
+    + url地址数据：computer-url.json
+    + 未剔除无效数据的数据：computer-meta.json
+    + 剔除无效数据的数据：computer.json
+
+
++ 小说类数据
+    + url地址数据：xiaoshuo-url.json
+    + 未剔除无效数据的数据：xiaoshuo-meta.json
+    + 剔除无效数据的数据：xiaoshuo.json    
---
+++ format.js
@@ -0,0 +1,54 @@
+//var metas = require('./computer-meta.json');
+var metas = require('./xiaoshuo-meta.json');
+
+var data = [];
+var xxx = function (str) {
+    str = str.replace('¥ ', '') || 0;
+    return +str;
+};
+var yyy = function (str) {
+    var res = {};
+    var strs = str.split('万字');
+
+    if (strs.length < 2) {
+        res.size = parseFloat(strs[0]);
+        return res;
+    }
+
+    if (strs[0]) {
+        res.number = +strs[0];
+    }
+    if (strs[1]) {
+        res.size = parseFloat(strs[1]);
+    }
+    return res;
+};
+console.log(metas.length);
+metas.filter(function (ele, index) {
+    return ele.currentPrice && ele.oldPrice && ele.paperPrice;
+}).map(function (ele, index) {
+    var n_s = yyy(ele.size);
+    var one = {
+        name: ele.name,
+        price: xxx(ele.currentPrice),
+        oldPrice: xxx(ele.oldPrice),
+        paperPrice: xxx(ele.paperPrice),
+        score: +ele.score,
+        count: +ele.count,
+    };
+    if (n_s.number) {
+        one.number = n_s.number;
+    }
+    if (n_s.size) {
+        one.size = n_s.size;
+    }
+    if (ele.tags.length > 0) {
+        one.tags = ele.tags;
+    }
+    data.push(one);
+});
+console.log(data.length);
+
+//require('fs').writeFileSync('computer.json', JSON.stringify(data), 'utf8');
+
+require('fs').writeFileSync('xiaoshuo.json', JSON.stringify(data), 'utf8');
